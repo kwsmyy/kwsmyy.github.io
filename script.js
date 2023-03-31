a5118d2202ffa5b6b537081340a93df15dd4b6c9
YuyaKawashima <kawashimayuya@gmail.com>
2023-03-31 17:16:42 +0900

    修正

--- script.js
+++ script.js
@@ -11,30 +11,42 @@ const contact = document.getElementById('contact');
 const contactPage = document.getElementById('contactpage');
 const contactCloseBtn = document.getElementById('contactclosebtn');
 
-menuBtn.addEventListener('click', openmenu);
+menuBtn.addEventListener('click', openMenu);
 
-profile.addEventListener('click', openprofile);
-profileCloseBtn.addEventListener('click', closeprofile);
+profile.addEventListener('click', openProfile);
+profileCloseBtn.addEventListener('click', closeProfile);
 
-contact.addEventListener('click', opencontact);
-contactCloseBtn.addEventListener('click', closecontact);
+contact.addEventListener('click', openContact);
+contactCloseBtn.addEventListener('click', closeContact);
 
-function openmenu(){
+window.addEventListener('load',openingAnimation);
+
+function openMenu(){
     menu.classList.toggle('openmenu');
 }
 
-function openprofile(){
+function openProfile(){
     profilePage.classList.add('openprofile');
 }
 
-function closeprofile(){
+function closeProfile(){
     profilePage.classList.remove('openprofile');
 }
 
-function opencontact(){
+function openContact(){
     contactPage.classList.add('opencontact');
 }
 
-function closecontact(){
+function closeContact(){
     contactPage.classList.remove('opencontact');
+}
+
+function openingAnimation(){
+    const opening = document.getElementById('opening');
+    setTimeout(function(){
+        opening.classList.add('end');
+        setTimeout(function(){
+            opening.classList.remove('active');
+        },1100)
+    },1600)
 }
